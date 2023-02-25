2e1345046667a860ccb4b83c707f6cc774fe0e3c
RayXu6 <3521838694@qq.com>
2023-02-25 21:28:47 +0800

    commit

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,40 @@
 export default {
     title: 'Speedrun Document',
     lastUpdated: true,
+    srcDir: './mcsr',
+    cleanUrls:true,
+    themeConfig: {
+        nav: [
+            {
+                text:"MCSR",
+                link:"/mcsr/"
+            },
+            {
+                text:"Socials",
+                items:[
+                    {text:"BiliBili",link:"https://space.bilibili.com/556631393"},
+                    {text:"Github",link:"https://github.com/RayXu6"}
+                ]
+            }
+        ],
+        sidebar: [
+            {
+                text: "总则",
+                link:"/"
+            },
+            {
+                text:"资源整理",
+                items:[
+                    {text:"关于访问Github加速",link:"/resources/steampp.md"},
+                    {text:"练习图",link:"/resources/pracmaps.md"},
+                    {text:"SSG种子合集",link:"/resources/ssgseeds.md"},
+                    {text:"有用的图片",link:"/resources/pics.md"},
+                ]
+            }
+        ],
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2023-present RayXu6'
+        }
+    }
 }
---
+++ docs/mcsr/resources/steampp.md
@@ -0,0 +1,6 @@
+## 关于访问Github加速
+国内访问Github目前时处于半墙状态的，就是有的时候可以进有的时候进不去    
+那么我是比较推荐使用Watt Toolkit的 也就是原来的Steam++
+下载方式: [官网](https://steampp.net/download)  或  Microsoft Store搜索Watt Toolkit   
+下载安装然后加速就可以了   
+其实这个原理就是改host实现加速
