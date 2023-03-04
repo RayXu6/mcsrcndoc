a991419e6fcfcb4afdfad633b248218f4acd4b14
RayXu6 <3521838694@qq.com>
2023-03-04 22:08:49 +0800

    commit

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -31,9 +31,23 @@ export default {
                     {text:"SSG种子合集",link:"/resources/ssgseeds.md"},
                     {text:"有用的图片",link:"/resources/pics.md"},
                     {text:"模组",link:"/resources/mods.md"},
-                    {text:"宏/工具",link:"/resources/tools.md"},
-                    {text:"关于访问Github加速",link:"/resources/steampp.md"},
+                    {text:"宏/工具",link:"/resources/tools.md"}
                 ]
+            },
+            {
+                text: "教程",
+                collapsed: true,
+                items: [
+                    {text: "如何使用SpawnJuicer进行SSG的多开",link: "/guide/spawnjuicer.md"},
+                    {text: "如何使用墙进行RSG的多开",link: "/guide/wall.md"},
+                    {text: "如何游玩排位模式",link: "/guide/mcsrranked.md"},
+                    {text: "如何配置用于k4hr挑战的计时器",link: "/guide/k4hr.md"},
+                    {text:"关于访问Github加速",link:"/guide/steampp.md"},
+                ]
+            },
+            {
+                text: "常见问题以及回答",
+                link: "/questions.md"
             }
         ],
         footer: {
