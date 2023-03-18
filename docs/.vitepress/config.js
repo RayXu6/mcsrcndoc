9e3e7f5fef6893c0df597d56f46cd1c887bfeb14
RayXu6 <3521838694@qq.com>
2023-03-18 20:41:52 +0800

    commit

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,10 +3,10 @@ export default {
     lastUpdated: true,
     srcDir: './mcsr',
     cleanUrls:true,
+    head:[
+        ['link', { rel: 'icon', href: '/icon.png' }]
+    ],
     themeConfig: {
-        head:[
-            ['link', { rel: 'icon', href: '/favicon.ico' }]
-        ],
         nav: [
             {
                 text:"MCSR",
@@ -22,8 +22,15 @@ export default {
         ],
         sidebar: [
             {
-                text: "总则",
-                link:"/"
+                text:"总则",
+                collapsed:false,
+                items:[
+                    {
+                    text: "前言",
+                    link:"/"
+                    }
+                ]
+
             },
             {
                 text:"资源整理",
@@ -32,9 +39,7 @@ export default {
                     {text:"排行榜及群",link:"/resources/leaderboard.md"},
                     {text:"练习图",link:"/resources/pracmaps.md"},
                     {text:"SSG种子合集",link:"/resources/ssgseeds.md"},
-                    {text:"有用的图片",link:"/resources/pics.md"},
                     {text:"模组",link:"/resources/mods.md"},
-                    {text:"宏/工具",link:"/resources/tools.md"}
                 ]
             },
             {
@@ -45,19 +50,37 @@ export default {
                         text:"游戏前的准备",
                         collapsed:true,
                         items: [
-                            {text:"关于启动器",link:""},
-                            {text:"如何安装模组",link:""},
-                            {text:"关于启动器",link:""},
-                            {text:"关于启动器",link:""},
-                            {text:"关于启动器",link:""},
+                            {text:"关于启动器",link:"/guide/launchers"},
+                            {text:"如何安装模组",link:"/guide/loader"}
                         ]
                     },
-                    {text: "如何使用SpawnJuicer进行SSG的多开",link: "/guide/spawnjuicer.md"},
-                    {text: "如何使用墙进行RSG的多开",link: "/guide/wall.md"},
-                    {text: "如何游玩排位模式",link: "/guide/mcsrranked.md"},
-                    {text: "如何配置用于k4hr挑战的计时器",link: "/guide/k4hr.md"},
-                    {text: "如何寻找一个种子的最佳出生点",link: "/guide/spawnpoint.md"},
-                    {text:"关于访问Github加速",link:"/guide/steampp.md"},
+                    {
+                        text:"游戏内技巧/知识",
+                        collapsed:true,
+                        items: [
+                            {text:"如何炸龙",link:""},
+                            {text:"如何无图找宝藏",link:""}
+                        ]
+                    },
+                    {
+                        text:"宏/工具的使用",
+                        collapsed:true,
+                        items: [
+                            {text: "前言",link: "/guide/macros"},
+                            {text: "使用SpawnJuicer进行SSG的多开",link: "/guide/spawnjuicer.md"},
+                            {text: "使用墙进行RSG的多开",link: "/guide/wall.md"},
+                        ]
+                    },
+                    {
+                        text:"杂项",
+                        collapsed:true,
+                        items: [
+                            {text: "如何游玩排位模式",link: "/guide/mcsrranked.md"},
+                            {text: "如何配置用于k4hr挑战的计时器",link: "/guide/k4hr.md"},
+                            {text: "如何寻找一个种子的最佳出生点",link: "/guide/spawnpoint.md"},
+                            {text:"关于访问Github加速",link:"/guide/steampp.md"},
+                        ]
+                    },
                 ]
             },
             {
